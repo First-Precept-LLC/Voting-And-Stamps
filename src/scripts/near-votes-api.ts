1f49c1c022409dff0055790ba5e1aa43b9624ced
daniablank <danielblank@berkeley.edu>
2022-07-02 14:56:41 -0400

    Get users and stamps from database

--- src/scripts/near-votes-api.ts
+++ src/scripts/near-votes-api.ts
@@ -1,9 +1,16 @@
 import * as nearAPI from "near-api-js";
 
+import { StampsModule } from "~/pages/api/graphql";
+
+import { gql, useMutation, useQuery, NetworkStatus } from '@apollo/client'
+
+
 const {connect, keyStores} = nearAPI;
 
 const keyStore = new keyStores.BrowserLocalStorageKeyStore(); //TODO: get a different key storage?
 
+
+let SAMPLE_VALUE = "Life" //TODO: change this
 
 const config = {
     networkId: "testnet",
@@ -28,3 +35,37 @@ const contract = new nearAPI.Contract(
       changeMethods: ["fullOverride"], // change methods modify state
     }
   );
+
+
+let stamps = new StampsModule();
+
+let getUserStamps = async (user, graph) => {
+    await stamps.init();
+    let resultData = await stamps.get_user_stamps(user, graph);
+    return resultData;
+}
+
+const GET_USERS = gql`
+  query vulcanUsers() {
+    vulcanUsers(input: {}) {
+      results {_id}
+    }
+  }`;
+
+  const {loading: queryLoading, data: queryData, error: queryError, refetch} = useQuery(
+    GET_USERS,
+    {
+      notifyOnNetworkStatusChange: true
+    }
+  )
+
+  let userIds = [] as any;
+  let stampCounts = [] as any;
+
+  for(let user in queryData["users"]["results"]) {
+      userIds.push(user);
+      stampCounts.push(getUserStamps(user, SAMPLE_VALUE));
+
+  }
+
+
