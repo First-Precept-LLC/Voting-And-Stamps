96ebbf313d73d9d4ed4aed730f08babcd694cb01
daniablank <danielblank@berkeley.edu>
2022-11-19 21:00:12 -0500

    Write value adding function

---
+++ src/pages/api/value-utils.ts
@@ -0,0 +1,73 @@
+
+import { gql, useMutation, useQuery, NetworkStatus } from '@apollo/client'
+
+let VALUE_UPDATE = gql`
+mutation updateOrg($id: String!, $valuesList: [String]) {
+    updateOrg(input: {id: "62cd9b45cc74b602dde8ce16", data: {valuesList: $valuesList}}) {
+       data {values {title}}
+     }
+ }
+`;
+
+const GET_ORG = gql`
+query org($id: String!) {
+  org(input: {id: $id}) {
+    result {valuesList}
+  }
+}`;
+
+const CREATE_VALUE = gql`
+mutation createValue($title: String!, $description: String!, $icon: String) {
+    createValue(input: {data: {title: $title, description: $description, icon: $icon}}) {
+        data { _id}
+    }
+}
+`
+
+
+
+
+
+export function createAndAddValue(org_id, value_title, value_desc, value_icon) {
+
+
+
+    let valuesList = [] as any;
+
+    let [addValue, {loading, data, error}] = useMutation(
+        VALUE_UPDATE,{
+          onCompleted: (data) => console.log("Data from mutation", data),
+          onError: (error) => console.error("Error creating a post", error),
+        }
+      );
+
+
+    let [createValue, {data: createData}] = useMutation(
+        CREATE_VALUE,{
+          onCompleted: (data) => addValue({variables: {id: org_id, valuesList: [data["createValue"]["data"]["_id"], ...valuesList]}}),
+      onError: (error) => console.error("Error creating a post", error),
+        }
+      );
+
+    const {loading: queryLoading, data: queryData, error: queryError, refetch} = useQuery(
+        GET_ORG,
+        {
+          notifyOnNetworkStatusChange: true,
+          variables: org_id,
+          onCompleted: (data => {
+            valuesList = data["org"]["result"]["valuesList"];
+            createValue({variables: {title: value_title, decription: value_desc, icon: value_icon}});
+          })
+        }
+    );
+
+     
+
+    
+    
+      
+    
+
+
+
+}
